8118898108fe5ff15afe994b069584d6427e137b
LosStockers <133064408+LosStockers@users.noreply.github.com>
2023-05-24 19:57:27 -0300

    Login/reg
    funciona, ¿como? ni idea

--- 123.php
+++ 123.php
@@ -1,4 +1,6 @@
-<?php
+<?php 
+
+
 
 $dbhost = "localhost";
 $dbuser = "root";
@@ -11,14 +13,20 @@ if (!$conn){
     die("No hay sistema mi estimado " .mysqli_connect_error()); 
 }
 
+
+
+
+
 // Login
 if (isset($_POST["btnlogin"])) {
+    
     $email = $_POST["correo"]; 
     $pass = $_POST["pass"];
 
     $query = mysqli_query($conn, "SELECT * FROM Login WHERE Email = '$email' AND Contraseña = '$pass'");
     $nfila = mysqli_num_rows($query);
 
+    
     if ($nfila == 1) {
         echo "<script> alert('Bienvenido'); window.location='Test.html'; </script>";
     } else {
@@ -28,31 +36,34 @@ if (isset($_POST["btnlogin"])) {
 
 // Register
 if (isset($_POST["btnregistrar"])) {
+    
     $usuario = $_POST["Usuario"];
     $emailreg = $_POST["correoreg"]; 
     $passreg = $_POST["passreg"];
 
     // Consulta para verificar si el correo electrónico ya existe
-    $query = mysqli_query($conn, "SELECT COUNT(*) AS count FROM Login WHERE Email = '$emailreg'");
-    $result = mysqli_fetch_assoc($query);
-    $count = $result['count'];
+$query = "SELECT COUNT(*) AS count FROM Login WHERE Email = :email";
+$stmt = $pdo->prepare($query);
+$stmt->bindParam(':email', $emailreg);
+$stmt->execute();
 
-    if ($count > 0) {
-        // El correo electrónico ya existe en la base de datos
-        // Puedes mostrar un mensaje de error o redirigir al usuario a otra página
-        echo "<script> alert('El correo electrónico ya está registrado. Por favor, utiliza otro correo electrónico.'); window.location='index.html'; </script>";
-        
-        
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+$count = $result['count'];
 
-    }
+if ($count > 0) {
+    // El correo electrónico ya existe en la base de datos
+    // Puedes mostrar un mensaje de error o redirigir al usuario a otra página
+    echo "El correo electrónico ya está registrado. Por favor, utiliza otro correo electrónico.";
+    exit;
+}
 
     $sqlgrabar = "INSERT INTO Login(Email, Contraseña, Usuario) VALUES ('$emailreg', '$passreg', '$usuario')";
 
     if (mysqli_query($conn, $sqlgrabar)) {
         echo "<script> alert('Registro exitoso'); window.location='Test.html'; </script>";
     } else {
-        echo "Error: " . $sqlgrabar . "<br>" . mysqli_error($conn);
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
-}
 
+}
 ?>
