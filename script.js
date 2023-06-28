cae41fe8ed5ad08dfb45d88e74ee52e1916c4685
LosStockers <emeraldstockers@gmail.com>
2023-06-28 02:55:04 -0300

    zz
    hice de todo en esto, de acá para atrás era la prueba gratis

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-//este llama a la constante login
-const login = document.querySelector('.login')
-//llama a la constante para loguear
-const loginLink = document.querySelector('.link-iniciar')
-//llama a la constante para registrar
-const registerLink = document.querySelector('.link-registro')
-//llama a la constante para usar el boton de login grande de arriba
-const btnPopup = document.querySelector('.btnlogin-popup')
-//esto es para usar la cruz de cerrado
-const iconclose = document.querySelector('.icono-cerrado')
+// Obtenemos todos los elementos con la clase 'gestor-info'
+const gestorInfoElements = document.querySelectorAll('.gestor-info');
 
+// Agregamos el evento 'mouseover' a cada elemento
+gestorInfoElements.forEach(element => {
+  element.addEventListener('mouseover', () => {
+    // Agrandamos el elemento actual de forma más lenta
+    element.style.transition = 'transform 0.3s ease';
+    element.style.transform = 'scale(1.1)';
+  });
 
-
-//esto lo que hace es activar el registrarse (usable en css)
-registerLink.addEventListener('click', () => {
-  login.classList.add('active');
+  element.addEventListener('mouseout', () => {
+    // Restauramos el tamaño original solo si el elemento no tiene el foco
+    if (!element.matches(':hover')) {
+      // Restauramos la transición a la velocidad original
+      element.style.transition = 'transform 0.5s ease';
+      element.style.transform = 'scale(1)';
+    }
+  });
 });
-
-//esto es para iniciar sesion (usable en css)
-loginLink.addEventListener('click', () => {
-  login.classList.remove('active');
-});
-//esto es para usar el boton grande de iniciar sesion de arriba (usable en css)
-btnPopup.addEventListener('click', () => {
-  login.classList.add('active-popup');
-})
-//esto es para poder modificar en css el boton (agrego un evento como en lo anteriores pero son casi las 5am y no tengo ganas de comentar otra vez las anteriores constantes)
-iconclose.addEventListener('click', () => {
-  login.classList.remove('active-popup')});
