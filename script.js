8ad9821b760f51fff7bfda96b51c5858161fa507
LosStockers <emeraldstockers@gmail.com>
2023-07-26 19:26:32 -0300

    sdfghnm
    borré todo

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const carrusel = document.querySelector('.carrusel');
+const imagenes = document.querySelectorAll('.imagen');
+
+
 //este llama a la constante Registrarse
 const Registro = document.querySelector('.Registro')
 //llama a la constante para loguear
@@ -15,3 +19,32 @@ Botoniniciar.addEventListener('click', () => {
 registerLink.addEventListener('click', () => {
   Registro.classList.remove('active-popup');
 });
+
+
+let contador = 0;
+
+function mostrarImagen(indice) {
+  imagenes.forEach((imagen, index) => {
+    imagen.style.transform = `translateX(${(index - indice) * 100}%)`;
+  });
+}
+
+function siguienteImagen() {
+  contador++;
+  if (contador === imagenes.length) {
+    contador = 0;
+  }
+  mostrarImagen(contador);
+}
+
+function imagenAnterior() {
+  contador--;
+  if (contador < 0) {
+    contador = imagenes.length - 1;
+  }
+  mostrarImagen(contador);
+}
+
+setInterval(siguienteImagen, 3000); // Cambia la imagen cada 3 segundos (puedes ajustar este valor)
+
+mostrarImagen(contador);
