d58427327b7d9e385c997b5da6cd393ea195ba8c
LosStockers <133064408+LosStockers@users.noreply.github.com>
2023-09-11 20:35:53 -0300

    intento1

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Vale">Vale</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/Vale.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="Vale">
+
+    </div>
+</template>
