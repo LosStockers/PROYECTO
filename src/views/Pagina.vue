d3257de181df2907454c3246bbada31b817f2529
LosStockers <133064408+LosStockers@users.noreply.github.com>
2023-10-17 20:44:21 -0300

    progreso

--- src/views/Pagina.vue
+++ src/views/Pagina.vue
@@ -1,53 +1,26 @@
-
 <template>
-  <div>
-    <v-btn
-      fab
-      dark
-      color="primary"
-      @click="agregarFoto"
-    >
-      <v-icon>mdi-camera</v-icon>
-    </v-btn>
+  <v-app>
+    <v-btn @click="navigateToInicio" class="navigate-button" color="primary">Ir a Inicio</v-btn>
+    <div class="todo">
 
-    <v-form>
-      <v-text-field
-        label="Nombre del producto"
-        v-model="nombre"
-      ></v-text-field>
-
-      <v-text-field
-        label="Precio del producto"
-        v-model="precio"
-      ></v-text-field>
-
-      <v-btn
-        color="primary"
-        @click="guardarProducto"
-      >
-        Guardar producto
-      </v-btn>
-    </v-form>
-  </div>
+    </div>
+  </v-app>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      nombre: '',
-      precio: '',
-    }
-  },
-
   methods: {
-    agregarFoto() {
-      // TODO: Implementar la lógica para agregar una foto
-    },
-
-    guardarProducto() {
-      // TODO: Implementar la lógica para guardar el producto
-    },
-  },
-}
+    navigateToInicio() {
+      this.$router.push({ name: "Inicio" });
+    }
+  }
+};
 </script>
+
+<style scoped>
+.navigate-button {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+}
+</style>
